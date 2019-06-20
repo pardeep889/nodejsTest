ad0d9a8d4fc41b4611ae65d6ffb96ef78ded30bb
pardeep889 <pardeep889@hotmail.com>
2019-06-20 16:35:39 +0530

    completed

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ app.use(express.static(__dirname + '/views'));
 app.get('/', (req,res) => {
     res.render(__dirname+'/views/');
 })
-
 app.get('/postData', (req,res) =>{
-    const data = req.query;  
     const plans = req.query.plans.split(',');
-    // console.log(plans);
-    let total = plans.reduce((prev,next) => parseInt(prev)+parseInt(next) );    
-    res.render(__dirname+'/views/showdata', {total});
+    const data = {};
+    let total = plans.reduce((prev,next) => parseInt(prev)+parseInt(next) );  
+    data.total = total;
+    data.fname = req.query.fname;
+    data.lname = req.query.lname;
+    data.id = req.query.id;
+    data.date = req.query.Date;
+    res.render(__dirname+'/views/showdata', {data});
 })
 
 app.listen(3000, () => {
